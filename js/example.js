143da72921b75a4fff31d85bd4443ec76c753b7a
Hypecraft <marshall@hypecraft.io>
2016-03-29 19:49:48 -0400

    added a fancy demo file structure with special effect and all

--- js/example.js
+++ js/example.js
@@ -2,9 +2,17 @@
 var namesList = [];
 var html = '';
 
+$("html").hide(0).fadeIn(1000);
+
 for ( i = 0; i < 7; i += 1 ) {
   namesList.push(chance.first());
-  html += '<li>' + oxen(namesList) + '</li>';
+  html = '<li>' + oxen(namesList) + '</li>';
+  $('.examples').append(html);
 }
 
-document.getElementById('examples').innerHTML = html;
+$('.examples li').hide().delay(500);
+
+$(function() {
+	$('.examples li').each(function(i) {
+	$(this).delay((i++) * 500).fadeTo(1000, 1); })
+});
